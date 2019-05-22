9eaaf0cc1933c2bbde66eb4326e7d7ed7ce3dba3
Mallhew <mah2001@iolani.org>
2019-05-21 16:38:14 -1000

    Code for the voice recognition to get numbers and add and subtract them

---
+++ theprogram.py
@@ -0,0 +1,43 @@
+import aiy.audio
+import aiy.cloudspeech
+import aiy.voicehat
+
+number=0 #default number
+
+def main():
+    recognizer = aiy.cloudspeech.get_recognizer()
+	for i in range(256):
+        toString = str(i)
+    	recognizer.expect_phrase('Set '+toString)
+	for i in range(256):
+		toString = str(i)
+	    recognizer.expect_phrase('Add '+toString)
+	for i in range(256):
+		toString = str(i)
+		recognizer.expect_phrase('Subtract' + toString)
+
+    aiy.audio.get_recorder().start()
+
+    while True:
+        print('Press the button and speak')
+        button.wait_for_press()
+        print('Listening...')
+        text = recognizer.recognize()
+        if not text:
+            print('Sorry, I did not hear you.')
+        else:
+			for i in range(256):
+				toString = str(i)
+				if 'Set '+toString in text:
+				    number = i
+			for i in range(256):
+				toString = str(i)
+				if 'Add '+toString in text:
+					number += i
+			for i in range(256):
+				if 'Subtract '+toString in text:
+					number -= i
+        print number
+
+if __name__ == '__main__':
+    main()
