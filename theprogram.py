b494eebbfde51e547bde91ed7e8f1e396acb9ebd
Mallhew <mah2001@iolani.org>
2019-05-22 08:21:32 -1000

    Reformatted code

--- theprogram.py
+++ theprogram.py
@@ -2,15 +2,15 @@ import aiy.audio
 import aiy.cloudspeech
 import aiy.voicehat
 
-number=0 #default number
+number = 0 #default number
 
 def main():
     recognizer = aiy.cloudspeech.get_recognizer()
-	for i in range(256):
+    for i in range(256):
         toString = str(i)
-    	recognizer.expect_phrase('Set '+toString)
-	    recognizer.expect_phrase('Add '+toString)
-		recognizer.expect_phrase('Subtract ' + toString)
+        recognizer.expect_phrase('Set '+toString)
+        recognizer.expect_phrase('Add '+toString)
+        recognizer.expect_phrase('Subtract ' + toString)
 
     aiy.audio.get_recorder().start()
 
@@ -22,14 +22,18 @@ def main():
         if not text:
             print('Sorry, I did not hear you.')
         else:
-			for i in range(256):
-				toString = str(i)
+            for i in range(256):
+                toString = str(i)
 				if 'Set '+toString in text:
-				    number = i
+                    number = i
 				if 'Add '+toString in text:
-				    number += i
-				if 'Subtract '+toString in text:
-				    number -= i
+                    number += i
+                if 'Subtract '+toString in text:
+                    number -= i
+                if number > 255
+                    number = 255
+				elif number < 0
+                    number = 0
         print number
 
 if __name__ == '__main__':
