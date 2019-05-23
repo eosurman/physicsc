4d75633297bee664ac903a50f6b2cda91e7a67b0
ewh1901 <ewh1901@iolani.org>
2019-05-22 16:28:06 -1000

    done

--- theprogram.py
+++ theprogram.py
@@ -1,6 +1,18 @@
 import aiy.audio
 import aiy.cloudspeech
 import aiy.voicehat
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(19,GPIO.OUT)
+GPIO.setup(21,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(26,GPIO.OUT)
+GPIO.setup(8,GPIO.OUT)
+GPIO.setup(10,GPIO.OUT)
+GPIO.setup(5,GPIO.OUT)
+GPIO.setup(3,GPIO.OUT)
  
 number=0
 def main():
@@ -35,3 +47,39 @@ while True:
  
 if __name__ == '__main__':
  main()
+ 
+#pi Zero WH
+#python
+#import math
+
+#if div >= 2**8:
+    #error
+#    e = 0
+#    x = 0
+#    while e < 2
+#        while x < 8
+#            GPIO.output(light[x][2], GPIO.HIGH)
+#            x = x + 1
+#        while x < 8
+#            GPIO.output(light[x][2], GPIO.LOW)
+#        e = e + 1
+x = 0 #counting up for array location
+n = 7 #count down for base 2
+number = 50   # number
+light = [0,0,0,0,0,0,0,0]
+pin = [3, 5, 19, 21, 23, 8, 10, 26]
+
+while x < 8: #going through binary digits
+ max = 2 ** (n) #binary base-2
+ #print (max)
+ if number >= max: #if divisible
+  number = number % max #then save the remainder
+  light[x] = 1
+  GPIO.output(pin[x], GPIO.HIGH)
+ else:
+  light[x] = 0
+  GPIO.output(pin[x], GPIO.LOW)
+ x = x + 1
+ n = n - 1
+print (light)
+
