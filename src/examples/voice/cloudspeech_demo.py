92a108e493872724e52412c216c0a5c37922642e
scn1901 <33670707+scn1901@users.noreply.github.com>
2019-05-22 19:03:54 -1000

    Update cloudspeech_demo.py

--- src/examples/voice/cloudspeech_demo.py
+++ src/examples/voice/cloudspeech_demo.py
@@ -104,14 +104,14 @@ GPIO.setup(26, GPIO.OUT)
 #inc and dec buttons
 input_inc = GPIO.input(11)
 input_dec = GPIO.input(12)
-if input_inc == False:
-    number = number + 1
+if input_inc == False: 
+    number = number + 1 #increase number for 8 bit
 if input_dec == False:
-    number = number - 1
+    number = number - 1 #decrease number for 8 bit
 
 #i'm not sure if this code works...
 if div >= 2**8:
-    #error
+    #if error exists
     n = 0 #first while loop b/c pins light up twice
     x = 0 #second while loop to turn on lights and then turn off
     while n < 2
@@ -124,11 +124,11 @@ if div >= 2**8:
         n = n + 1
 
 #potential pins
-#pin = [3, 8, 11, 15, 18, 19, 23, 26]
+#NA pin = [3, 8, 11, 15, 18, 19, 23, 26]
 
 x = 0 #counting up for array location
 n = 7 #count down for base 2
-#div = 95    number
+#div = 95    testing number
 light = [0,0,0,0,0,0,0,0] #empty binary array
 pin = [3, 5, 19, 21, 23, 8, 10, 26] #pin location
 
@@ -138,10 +138,10 @@ while x < 8: #going through binary digits
     if number >= max: #if divisible
         number = number % max #then save the remainder
         light[x] = 1
-        GPIO.output(light[x], GPIO.HIGH)
+        GPIO.output(light[x], GPIO.HIGH) #if this bit is 1, light will light
     else:
         light[x] = 0
-        GPIO.output(light[x], GPIO.LOW)
+        GPIO.output(light[x], GPIO.LOW) #if this bit is 0, light wont light
     x = x + 1
     n = n - 1
 print (light)
